a2a0b1f693059e34419e259314f5c4c1ec37aae5
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-01-19 20:30:04 -0600

    progress on integration tests

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -8,7 +8,24 @@ void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   testWidgets('Page1', (WidgetTester tester) async {
-    //TODO: Finish this based on test/main_test.dart.
+    app.main();
+    await tester.pumpAndSettle();
+
+    // Find widgets.
+    var shoutBtn = find.byKey(ValueKey('shoutBtn'));
+    var nameField = find.byKey(ValueKey('nameField'));
+
+    // Verify the contents of the first page.
+    expect(find.text('This is page #1.'), findsOneWidget);
+
+    var name = 'Mark';
+    await tester.enterText(nameField, name);
+    await tester.pumpAndSettle();
+    expect(find.text('Hello, $name!'), findsOneWidget);
+
+    await tester.tap(shoutBtn);
+    await tester.pumpAndSettle();
+    expect(find.text('Hello, ${name.toUpperCase()}!'), findsOneWidget);
   });
 
   testWidgets('forward and back buttons', (WidgetTester tester) async {
@@ -32,10 +49,51 @@ void main() {
   });
 
   testWidgets('swipe left and right', (WidgetTester tester) async {
-    //TODO: Finish this based on test/main_test.dart.
+    Future<void> swipe(page, swipeLeft, expectedText) async {
+      //TODO: How can we get the device width in a test?
+      var pSize = tester.binding.window.physicalSize;
+      tester.printToConsole('pSize = $pSize');
+      var offset = Offset(pSize.width, pSize.height);
+      tester.printToConsole(
+          'local size = ${tester.binding.globalToLocal(offset)}');
+      double deviceWidth = 600; // MediaQuery.of(context).size.width;
+
+      offset = Offset(deviceWidth * (swipeLeft ? 1 : -1), 0);
+      var speed = 300.0; // pixels per second
+      await tester.fling(page, offset, speed);
+      await tester.pumpAndSettle();
+
+      expect(find.text(expectedText), findsOneWidget);
+    }
+
+    app.main();
+    await tester.pumpAndSettle();
+
+    var page1 = find.byKey(ValueKey('page1'));
+    var page2 = find.byKey(ValueKey('page2'));
+    var page3 = find.byKey(ValueKey('page3'));
+
+    await swipe(page1, false, 'This is page #2.'); // goes forward
+    await swipe(page2, false, 'This is page #3.'); // goes forward
+    await swipe(page3, false, 'This is page #3.'); // stays on same page
+    await swipe(page3, true, 'This is page #2.'); // goes backward
+    await swipe(page2, true, 'This is page #1.'); // goes backward
+    await swipe(page1, true, 'This is page #1.'); // stays on same page
   });
 
   testWidgets('tap dots', (WidgetTester tester) async {
-    //TODO: Finish this based on test/main_test.dart.
+    Future<void> tapDot(int number) async {
+      var dot = find.byKey(ValueKey('dot$number'));
+      await tester.tap(dot);
+      await tester.pumpAndSettle();
+      expect(find.text('This is page #$number.'), findsOneWidget);
+    }
+
+    app.main();
+    await tester.pumpAndSettle();
+
+    for (var number = 1; number <= 3; number++) {
+      await tapDot(number);
+    }
   });
 }
